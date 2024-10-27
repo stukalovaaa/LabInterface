937214f3e00553a38b39253f89fa40529792b5f7
stukalovaaa <sasha.stukalowa@gmail.com>
2024-10-27 17:08:41 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
+
 let item = "";
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+
 btn1.addEventListener("click", function () {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
@@ -15,16 +19,6 @@ btn1.addEventListener("click", function () {
         tg.MainButton.show();
     }
 });
-btn3.addEventListener("click", function () {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    }
-    else {
-        tg.MainButton.setText("Вывести информацию о подковоносе");
-        item = "podkovonos";
-        tg.MainButton.show();
-    }
-});
 btn2.addEventListener("click", function () {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
@@ -32,6 +26,16 @@ btn2.addEventListener("click", function () {
     else {
         tg.MainButton.setText("Вывести информацию об амурском тигре");
         item = "amurtigr";
+        tg.MainButton.show();
+    }
+});
+btn3.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию о подковоносе");
+        item = "podkovonos";
         tg.MainButton.show();
     }
 });
